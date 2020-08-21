651aa441fab49f23dac0dae7e1352ace9191b7bc
draganrakita <dragan0rakita@gmail.com>
2020-08-21 11:41:53 +0200

    Docker scripts updated

--- scripts/docker/hub/check_sync.sh
+++ scripts/docker/hub/check_sync.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-# checks if Open Ethereum has a fully synced blockchain
+# checks if OpenEthereum has a fully synced blockchain
 
 ETH_SYNCING=$(curl -X POST --data '{"jsonrpc":"2.0","method":"eth_syncing","params":[],"id":1}' http://localhost:8545 -H 'Content-Type: application/json')
 RESULT=$(echo "$ETH_SYNCING" | jq -r .result)
 
 if [ "$RESULT" == "false" ]; then
-  echo "Open Ethereum is ready to start accepting traffic"
+  echo "OpenEthereum is ready to start accepting traffic"
   exit 0
 else
-  echo "Open Ethereum is still syncing the blockchain"
+  echo "OpenEthereum is still syncing the blockchain"
   exit 1
 fi
